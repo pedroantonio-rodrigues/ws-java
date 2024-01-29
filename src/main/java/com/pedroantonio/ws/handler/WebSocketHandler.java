b1d5c53ce67fc88073b4d1b967f7c9eb936a103f
Pedro Antonio <pedro_arxp@hotmail.com>
2024-01-29 20:03:45 -0300

    Start a project

---
+++ src/main/java/com/pedroantonio/ws/handler/WebSocketHandler.java
@@ -0,0 +1,9 @@
+package com.pedroantonio.ws.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+}
